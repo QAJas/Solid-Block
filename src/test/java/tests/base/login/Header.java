41b2ddde44416d9530a6c591e63dbe9de87bc31f
hasmikgevorgyan <hasmik.gevorgyan.gh@gmail.com>
2021-06-24 18:39:47 +1000

    news
    Signed-off-by: hasmikgevorgyan <hasmik.gevorgyan.gh@gmail.com>

--- src/test/java/tests/base/login/Header.java
+++ src/test/java/tests/base/login/Header.java
@@ -3,7 +3,7 @@ package tests.base.login;
 
 
 import constants.HomePageLocators;
-import helpers.WaitFor;
+import helpers.WaitFor ;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import tests.base.BaseTest;
