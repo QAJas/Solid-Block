277e50947a9ea4b8fdf9f6eee8bbdadb711c36a5
qaJas <tatevqalashyan21@gmail.com>
2021-06-25 13:10:39 +1000

    nn
    Signed-off-by: qaJas <tatevqalashyan21@gmail.com>

--- src/test/java/constants/driver/ProfieLocators.java
+++ src/test/java/constants/driver/ProfieLocators.java
@@ -6,7 +6,7 @@ public class ProfieLocators {
     public static final String PROFILE_NAME = "#firstName";
     public static final String PROFILE_LAST_NAME ="#lastName";
     public static final String COUNTRY = "button.MuiButtonBase-root.MuiIconButton-root.MuiAutocomplete-popupIndicator";
-    public static final String US_CITIZEN = "input.jss20";
+    public static final String US_CITIZEN = "input.jss11";
     public static final String STATE = "#state";
     public static final String CITY = "#city";
     public static final String ADDRESS = "#address";
