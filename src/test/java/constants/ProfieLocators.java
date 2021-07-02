3632404dc13e79e03b7eae2128eeb334b61358bd
Jas <tatevqalashyan21@gmail.com>
2021-07-02 17:22:17 +1000

    02.07.2021

--- src/test/java/constants/ProfieLocators.java
+++ src/test/java/constants/ProfieLocators.java
@@ -1,10 +1,10 @@
 package constants;
 
 public class ProfieLocators {
-    public static final String USER_ICON_IN_HEADER = "div.logout-icon-container";
+    public static final String USER_ICON_IN_HEADER = "div.logout-icon-container > img";
     public static final String PROFILE_ON_HOVER = "div.collapse-header-item:nth-child(1)";
     public static final String PROFILE_NAME = "#firstName";
-    public static final String PROFILE_LAST_NAME ="#lastName";
+    public static final String PROFILE_LAST_NAME = "#lastName";
     public static final String COUNTRY = "button.MuiButtonBase-root.MuiIconButton-root.MuiAutocomplete-popupIndicator";
     public static final String US_CITIZEN = "input.jss11";
     public static final String STATE = "#state";
@@ -18,10 +18,11 @@ public class ProfieLocators {
     public static final String DELETE_USER = "button.MuiButtonBase-root.MuiButton-root.MuiButton-text.jss2.button-delete-profile > span.MuiButton-label";
     public static final String SECURITY = "a.profile-nav__link:nth-child(2)";
     public static final String NEW_PASS = "body > div#root:nth-child(3) > main > div.security-page:nth-child(3) > section.security-page-content:nth-child(4) > form.security-form:nth-child(2) > div.MuiFormControl-root:nth-child(2) > div.MuiInputBase-root.MuiInput-root.MuiInput-underline.form-field.MuiInputBase-formControl.MuiInput-formControl.MuiInputBase-adornedEnd:nth-child(2) > input#auth-password.MuiInputBase-input.MuiInput-input.MuiInputBase-inputAdornedEnd";
-    public static final String CONFIRM_PASS ="body > div#root:nth-child(3) > main > div.security-page:nth-child(3) > section.security-page-content:nth-child(4) > form.security-form:nth-child(2) > div.MuiFormControl-root:nth-child(3) > div.MuiInputBase-root.MuiInput-root.MuiInput-underline.form-field.MuiInputBase-formControl.MuiInput-formControl.MuiInputBase-adornedEnd:nth-child(2) > input#auth-password.MuiInputBase-input.MuiInput-input.MuiInputBase-inputAdornedEnd";
+    public static final String CONFIRM_PASS = "body > div#root:nth-child(3) > main > div.security-page:nth-child(3) > section.security-page-content:nth-child(4) > form.security-form:nth-child(2) > div.MuiFormControl-root:nth-child(3) > div.MuiInputBase-root.MuiInput-root.MuiInput-underline.form-field.MuiInputBase-formControl.MuiInput-formControl.MuiInputBase-adornedEnd:nth-child(2) > input#auth-password.MuiInputBase-input.MuiInput-input.MuiInputBase-inputAdornedEnd";
     public static final String OLD_PASS = "body > div#root:nth-child(3) > main > div.security-page:nth-child(3) > section.security-page-content:nth-child(4) > form.security-form:nth-child(2) > div.security-input-container:nth-child(4) > div.MuiFormControl-root:nth-child(1) > div.MuiInputBase-root.MuiInput-root.MuiInput-underline.form-field.MuiInputBase-formControl.MuiInput-formControl.MuiInputBase-adornedEnd:nth-child(2) > input#auth-password.MuiInputBase-input.MuiInput-input.MuiInputBase-inputAdornedEnd";
     public static final String SAVE_PASS_CHANGE = "button.security-button";
     public static final String SUC_FOR_PASS = "body > div#root:nth-child(3) > main > div.security-page:nth-child(3) > section.security-page-content:nth-child(4) > span.input-success";
-public static final String PASS_1  = "654654654";
-public static final String PASS_2 = "321321321";
+    public static final String PASS_1 = "654654654";
+    public static final String PASS_2 = "321321321";
+    public static final String LOGOUT = "div.collapse-header-item";
 }
